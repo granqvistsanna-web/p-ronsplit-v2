0c611d799ec6897633935753e168c7c9696a7f92
Sanna Granqvist <granqvistsanna-web@users.noreply.github.com>
2026-01-28 17:33:34 +0100

    chore(04-01): export drill-down components from analytics barrel
    - Export TransactionList component
    - Export CategoryDrillDown component
    - Export SelectedCategory type

--- src/components/analytics/index.ts
+++ src/components/analytics/index.ts
@@ -3,3 +3,6 @@ export { CategoryDonut, CategoryLegend } from "./CategoryDonut";
 export { ComparisonBar } from "./ComparisonBar";
 export { CategoryBarChart } from "./CategoryBarChart";
 export { CategoryChartSection } from "./CategoryChartSection";
+export { TransactionList } from "./TransactionList";
+export { CategoryDrillDown } from "./CategoryDrillDown";
+export type { SelectedCategory } from "./CategoryDrillDown";
